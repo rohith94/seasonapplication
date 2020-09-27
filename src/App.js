5854d9076f40dfa515af40a905b1a2101454afbd
Rohith M <rohith.m94@gmail.com>
2020-09-27 19:47:49 +0530

    worked with state and onditional rendering

--- src/App.js
+++ src/App.js
@@ -14,16 +14,33 @@ import React from "react";
 // };
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: "" };
+
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         console.log(position);
+        this.setState({ lat: position.coords.latitude });
       },
       (err) => {
         console.log(err);
+        this.setState({ errorMessage: err.message });
       }
     );
-    return <div>Lattitude:</div>;
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error : {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Lattitude : {this.state.lat} </div>;
+    }
+
+    return <div>loading ...!</div>;
   }
 }
 
